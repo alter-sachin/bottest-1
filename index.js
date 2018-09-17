0951906a85f540e64c947dfb790ba69a860ffacf
Sachin <sachin@localhost.localdomain>
2018-09-17 12:47:51 +0530

    minor

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "this123";
+  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>";
     
   // Parse the query params
   let mode = req.query['hub.mode'];
